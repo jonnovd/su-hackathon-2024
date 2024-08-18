e42c50e9e3c5d55a6f04bc2759ab88c28fbcbe8e
DCoetzee <101389904+DCoetzee@users.noreply.github.com>
2024-08-18 23:33:57 +0200

    Update test_predictions.py

--- test_predictions.py
+++ test_predictions.py
@@ -12,34 +12,17 @@ from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler
 
 df_test = pd.read_csv('df_area_non_zero_test_mm.csv')
-
 df_test = df_test[df_test['Pothole number'] != 117]
-# df.columns = df.columns.str.strip()
 df = pd.read_csv('filtered_potholes_mm_out.csv')
-
-# df_test['ratio_width'] = df_test['width_0']/df_test['width_1']
-# df_test['ratio_height'] = df_test['height_0']/df_test['height_1']
-
-# df= df[df['pothole_area']!=0.0]
-# count_0_5 = df[df['Bags used'] == 0.5].shape[0]
-
-# # Determine how many rows to remove, for example, remove 50% of them
-# rows_to_remove = int(count_0_5 * 0.5)  # Adjust the percentage as needed
-
-# indices_to_remove = df[df['Bags used'] == 0.5].sample(n=rows_to_remove, random_state=42).index
-
-# df = df.drop(indices_to_remove)
 df.columns = df.columns.str.strip()
-
-# df['ratio_width'] = df['width_0']/df['width_1']
-# df['ratio_height'] = df['height_0']/df['height_1']
 
 X = df[['pothole_area']]
 y = df['Bags used']
 
-test=df_test[['pothole_area']]
+test = df_test[['pothole_area']]
 
-def svr(X, test, y_train, y_test, df_test):
+
+def svr(X_train, test, y_train, df_test):
     param_grid = {
         'C': [0.1, 1, 10, 100, 1000],
         'epsilon': [0.001, 0.01, 0.1, 1],
@@ -94,8 +77,6 @@ def polynomial_regression_model(X_train, X_test, y_train):
     
     result_df.to_csv('predicted_pothole_bags.csv', index=False)
 
-# svr(X_train, test, y_train, y_test, df_test)
-polynomial_regression_model(X, test, y)
 
 def linear_regression_model(X_train, X_test, y_train, y_test):
     model = LinearRegression()
@@ -108,8 +89,7 @@ def linear_regression_model(X_train, X_test, y_train, y_test):
     })
     
     result_df.to_csv('predicted_pothole_bags.csv', index=False)
-    
-# linear_regression_model(X_train, test, y_train, y_test)
+
 
 def gradient_boosting(X_train, X_test, y_train, y_test):
     param_grid = {
@@ -129,7 +109,6 @@ def gradient_boosting(X_train, X_test, y_train, y_test):
 
     y_hat = best_model.predict(X_test)
 
-
     result_df = pd.DataFrame({
         'Pothole number': df_test['Pothole number'],
         'Bags used': y_hat
@@ -137,6 +116,5 @@ def gradient_boosting(X_train, X_test, y_train, y_test):
     
     result_df.to_csv('predicted_pothole_bags.csv', index=False)
     
-# svr(X_train, test, y_train, y_test, df_test)
-
-
+    
+#svr(X_train, test, y_train, y_test, df_test)
